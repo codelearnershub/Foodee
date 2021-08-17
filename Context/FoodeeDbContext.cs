5ec5f2d533c9283fab7c4f13674574d64ceee9c0
Olowonmihabeebah <olowonmiadejoke111@gmail.com>
2021-08-17 18:10:03 +0100

    DbContext

---
+++ Context/FoodeeDbContext.cs
@@ -0,0 +1,33 @@
+﻿using FOODEE.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FOODEE.Context
+{
+    public class FoodeeDbContext: DbContext
+    {
+        public FoodeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Menu> Menu { get; set; }
+        public DbSet<MenuItem> MenuItem { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<UserRole> UserRole { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderItem>(o =>
+            {
+                o.HasIndex(o => new { o.MenuId, o.OrderId }).IsUnique();
+            });
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -9,10 +9,29 @@ namespace FOODEE.Models
 {
     public class Customer: BaseEntity
     {
+        [Required(ErrorMessage = "First Name is required")]
+        [DisplayName("First Name")]
+        [StringLength(60)]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [DisplayName("Last Name")]
+        [StringLength(60)]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(70)]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(24)]
         public string PhoneNumber { get; set; }
+
+        [DisplayName("Email Address")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+        ErrorMessage = "Email is is not valid.")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         public string Gender { get; set; }
         public virtual ICollection<Order> Order { get; set; } = new List<Order>();
