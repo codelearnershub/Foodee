5e11114472efdbd439fdb9455e33bd5d21562f33
Olowonmihabeebah <olowonmiadejoke111@gmail.com>
2021-08-18 17:42:07 +0100

    Repositories

---
+++ Repository/RoleRepository.cs
@@ -0,0 +1,48 @@
+﻿using FOODEE.Context;
+using FOODEE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FOODEE.Repository
+{
+    public class RoleRepository
+    {
+        private readonly FoodeeDbContext _dbContext;
+        public RoleRepository(FoodeeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Role Add(Role role)
+        {
+            _dbContext.Roles.Add(role);
+            _dbContext.SaveChanges();
+            return role;
+        }
+        public void Delete(int id)
+        {
+            var role = FindById(id);
+            if (role != null)
+            {
+                _dbContext.Roles.Remove(role);
+                _dbContext.SaveChanges();
+            }
+        }
+        public Role FindById(int id)
+        {
+            return _dbContext.Roles.Find(id);
+        }
+        public Role Update(Role role)
+        {
+            _dbContext.Roles.Update(role);
+            _dbContext.SaveChanges();
+            return role;
+        }
+        public List<Role> GetAll()
+        {
+            return _dbContext.Roles.ToList();
+
+        }
+    }
+}
---
+++ Repository/UserRoleRepository.cs
@@ -0,0 +1,53 @@
+﻿using FOODEE.Context;
+using FOODEE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FOODEE.Repository
+{
+    public class UserRoleRepository
+    {
+        private readonly FoodeeDbContext _dbContext;
+        public UserRoleRepository(FoodeeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public UserRole Add(UserRole userrole)
+        {
+            _dbContext.UserRoles.Add(userrole);
+            _dbContext.SaveChanges();
+            return userrole;
+        }
+        public UserRole FindById(int id)
+        {
+            return _dbContext.UserRoles.Find(id);
+        }
+
+        public void Delete(int id)
+        {
+            var userrole = FindById(id);
+            if (userrole != null)
+            {
+                _dbContext.UserRoles.Remove(userrole);
+                _dbContext.SaveChanges();
+            }
+        }
+        public UserRole Update(UserRole userrole)
+        {
+            _dbContext.UserRoles.Update(userrole);
+            _dbContext.SaveChanges();
+            return userrole;
+        }
+        public List<UserRole> GetAll()
+        {
+            return _dbContext.UserRoles.ToList();
+
+        }
+        public bool Exists(int id)
+        {
+            return _dbContext.UserRoles.Any(e => e.Id == id);
+        }
+    }
+}
